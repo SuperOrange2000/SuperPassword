5dd20ecb8eea7ccca2fc39abf2552a90793def4b
fusheng2000 <superorange@tom.com>
2024-03-24 12:47:28 +0800

    添加新的tag

--- SuperPassword/ViewModels/AddInfoGroupViewModel.cs
+++ SuperPassword/ViewModels/AddInfoGroupViewModel.cs
@@ -1,12 +1,16 @@
-﻿using Prism.Commands;
+﻿using Newtonsoft.Json.Linq;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Services.Dialogs;
 using SuperPassword.Entity.Data;
 using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
 
 namespace SuperPassword.ViewModels
 {
-    class AddInfoGroupViewModel : BindableBase, IDialogAware
+    class AddInfoGroupViewModel : BindableBase, IDialogAware, INotifyPropertyChanged
     {
         private InfoGroupEntity _model;
         public InfoGroupEntity Model
@@ -23,15 +27,49 @@ namespace SuperPassword.ViewModels
             set { _title = value; }
         }
 
+        private bool _isInputing = false;
+
+        public bool IsInputing 
+        { 
+            set 
+            { 
+                _isInputing = value; 
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(newTagButtonVisibility))); 
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(newTagInputBoxVisibility))); 
+            } 
+        }
+
+        public Visibility newTagButtonVisibility 
+        { 
+            get { return _isInputing ? Visibility.Collapsed : Visibility.Visible; } 
+        }
+        public Visibility newTagInputBoxVisibility 
+        { 
+            get { return _isInputing ? Visibility.Visible : Visibility.Collapsed; } 
+        }
+
+        private string _newTagText;
+
+        public string NewTagText
+        {
+            get { return _newTagText; }
+            set { _newTagText = value; RaisePropertyChanged(); }
+        }
+
+
         public string DialogHostName { get; set; }
         public DelegateCommand SaveCommand { get; set; }
         public DelegateCommand CancelCommand { get; set; }
+        public DelegateCommand StartInputingCommand { get; set; }
+        public DelegateCommand EndInputingCommand { get; set; }
 
 
         AddInfoGroupViewModel()
         {
             SaveCommand = new DelegateCommand(Save);
             CancelCommand = new DelegateCommand(Cancel);
+            StartInputingCommand = new DelegateCommand(StartInputing);
+            EndInputingCommand = new DelegateCommand(EndInputing);
         }
 
         private void Cancel()
@@ -50,6 +88,19 @@ namespace SuperPassword.ViewModels
                 { "Value", Model }
             };
             RequestClose.Invoke(new DialogResult(ButtonResult.OK, param));
+        }
+
+        private void StartInputing()
+        {
+            IsInputing = true;
+        }
+
+        private void EndInputing()
+        {
+            IsInputing = false;
+            byte maxId = Model.TagEntities.Max(t => t.NonceID);
+            Model.TagEntities.Add(new Entity.TagEntity(++maxId) { Salt = Model.Salt, Content=NewTagText });
+            NewTagText = string.Empty;
         }
 
         public event Action<IDialogResult> RequestClose;
